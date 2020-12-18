cb44f75a7a754b65dfb6b6519b44e735d6c2440f
lukehayes <lukepurplehayes@gmail.com>
2020-12-18 19:08:30 +0000

    Tidied up index.php

--- index.php
+++ index.php
@@ -5,29 +5,16 @@ use Migrate\ColumnType\ColumnType;
 use Migrate\Table\Table;
 use Migrate\Migration;
 
-
-$user_id = new ColumnType("id", "int", 10, ['AUTO_INCREMENT', 'NOT_NULL'] );
-$username = new ColumnType("username", "varchar", 255);
-$email = new ColumnType("email", "varchar", 255);
-$password = new ColumnType("password", "varchar", 255);
-
-echo $user_id;
-dump($username);
-dump($email);
-
 $usersTable = new Table('users');
-$usersTable->addColumn($username);
-$usersTable->addColumn($email);
-$usersTable->addColumn($password);
+$usersTable->addColumn(new ColumnType('id', 'integer', 100, ['AUTO_INCREMENT', 'NOT NULL', 'PRIMARY KEY']));
+$usersTable->addColumn(new ColumnType('username', 'varchar', 100, ['NOT NULL']));
+$usersTable->addColumn(new ColumnType('email', 'varchar', 500));
+$usersTable->addColumn(new ColumnType('password', 'varchar', 25));
 
 $postsTable = new Table('posts');
 $postsTable->addColumn(new ColumnType('title', 'varchar', 100));
 $postsTable->addColumn(new ColumnType('content', 'varchar', 500));
 $postsTable->addColumn(new ColumnType('posted_on', 'date'));
-
-dump($usersTable);
-dump($postsTable);
-
 
 $m = new Migration();
 $m->addTable($usersTable);
@@ -35,15 +22,6 @@ $m->addTable($postsTable);
 
 dump($m);
 dump($m->getTables());
-
-
-//$str = "";
-//$str .= ($m->genType($user_id));
-//$str .= ($m->genType($username));
-//$str .= ($m->genType($email));
-//$str .= ($m->genType($password));
-
-//$sql = "create table $postsTable->name (" . $str .");";
 
 $m->run();
 
